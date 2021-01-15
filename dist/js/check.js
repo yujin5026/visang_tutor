5e2185d35bd1040d7112796631047124fba54b68
yujin5026 <iikasam@ucomp.co.kr>
2021-01-15 09:43:25 +0900

    new project

--- dist/js/check.js
+++
@@ -1,7 +0,0 @@
-const weekBox = document.querySelector('.addclass_week_box');
-const checkBox = document.querySelector('.week_box_check');
-
-
-weekBox.onclick = function(){
-    weekBox.style.background="red";
-}
