2da636dffb2ba33c9c9de13f7964197a0d3de124
yujin5026 <iikasam@ucomp.co.kr>
2021-01-18 17:28:45 +0900

    KETA_MY_003 콘텐츠 완료

--- src/js/index.js
+++ src/js/index.js
@@ -157,7 +157,6 @@ $('ul.selector_tabs li').last().addClass("tab_last");
 
 
 
-
 // 모달 컨트롤
 function modalView(modalName) {
     var modalWidth = $(".modalpop .popupwrap." + modalName).innerWidth() / 2;
@@ -215,3 +214,44 @@ jQuery(document).ready(function($){
         }
     });
 });
+
+
+
+
+
+var starRating = function(){
+    var $star = $(".star-input"),
+        $result = $star.find("output>b");
+        
+          $(document)
+        .on("focusin", ".star-input>.input", 
+            function(){
+                $(this).addClass("focus");
+         })
+             
+           .on("focusout", ".star-input>.input", function(){
+            var $this = $(this);
+            setTimeout(function(){
+                  if($this.find(":focus").length === 0){
+                       $this.removeClass("focus");
+                  }
+               }, 100);
+          })
+      
+        .on("change", ".star-input :radio", function(){
+            $result.text($(this).next().text());
+          })
+        .on("mouseover", ".star-input label", function(){
+            $result.text($(this).text());
+        })
+        .on("mouseleave", ".star-input>.input", function(){
+            var $checked = $star.find(":checked");
+                if($checked.length === 0){
+                      $result.text("0");
+                    } else {
+                      $result.text($checked.next().text());
+                }
+          });
+    };
+    
+    starRating();
